75e3edcc37236e7d9d1f5a8139a6dc0f855c48cb
mickestiler <mickestiler@gmail.com>
2024-12-24 22:11:53 -0500

    changed ViewSteps

--- db-setup/src/test/java/com/revature/steps/ViewSteps.java
+++ db-setup/src/test/java/com/revature/steps/ViewSteps.java
@@ -1,18 +1,50 @@
-package org.example.steps;
+package com.revature.steps;
 
-import org.junit.BeforeClass;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import com.revature.TestRunner;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class ViewSteps {
 
-    public static WebDriver driver = null;
-    public static WebDriverWait wait;
-
-    @BeforeClass
-    public static void setup() {
-        driver = new ChromeDriver();
-
+    @Given("the user is logged in on the home page")
+    public void the_user_is_logged_in_on_the_home_page() {
+        TestRunner.loginPage.setUpLoggedInUser();
     }
+
+    @Given("the user is not logged in")
+    public void the_user_is_not_logged_in() {
+        TestRunner.loginPage.openLoginPage();
+    }
+
+    @When("the user tries to directly access the home page")
+    public void the_user_tries_to_directly_access_the_home_page() {
+        TestRunner.homePage.tryToAccessHomePageDirectly();
+    }
+
+    @Then("they should see their planets and moons")
+    public void they_should_see_their_planets_and_moons() {
+        // TODO: switch from try/finally to using a Cucumber hook to log out the user
+        try{
+            TestRunner.wait.until(ExpectedConditions.titleIs("Home"));
+            Assert.assertEquals(
+                    String.format(
+                            "Expected 'Welcome to the Home Page Batman, but got %s",
+                            TestRunner.homePage.getHomePageGreeting()
+                    ),
+                    "Welcome to the Home Page Batman",
+                    TestRunner.homePage.getHomePageGreeting());
+            Assert.assertEquals(4, TestRunner.homePage.getNumberOfCelestialRows());
+        } finally {
+            TestRunner.homePage.logout();
+        }
+    }
+
+    @Then("the user should be denied access")
+    public void the_user_should_be_denied_access() {
+        Assert.assertNotEquals("Home", TestRunner.driver.getTitle());
+    }
+
 }
